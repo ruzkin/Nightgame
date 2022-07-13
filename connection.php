bd48c5c8a076d5ec59fcaeb6c285b03f26425b0e
ruzkin <ruzkin111@gmail.com>
2022-07-13 10:19:13 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -7,10 +7,10 @@ $db_name = "ete33e_2122ls_06";
 */
 
 
-$host = "sql4.webzdarma.cz:3306";
-$user = "pivohodnocen4318";
-$password = '51..1@L$zhmlfbyg02G*';
-$db_name = "pivohodnocen4318";
+$host = "";
+$user = "";
+$password = '';
+$db_name = "";
 $conn = mysqli_connect($host, $user, $password, $db_name);
 if(mysqli_connect_errno()) {
     die("Failed to connect with MySQL: ". mysqli_connect_error());
